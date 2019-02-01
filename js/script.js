4f1c78bcacd3c82d1848241adb6eba22d5682b37
Stanislav Topalo <topalo@acmgroup.info>
2019-02-01 14:31:56 +0300

    P-1:Added header 100%

---
+++ js/script.js
@@ -0,0 +1,44 @@
+(function($){
+
+    $('.list-quote-js').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        arrows: false,
+        draggable: false,
+        lazyLoad: 'ondemand',
+        infinite: true,
+        fade: true,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 7000,
+        // responsive: [
+        //     {
+        //         breakpoint: 769,
+        //         settings: {
+        //             slidesToShow: 2,
+        //             slidesToScroll: 2
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 561,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1
+        //         }
+        //     }
+        // ]
+    });
+
+    // плавный скролл к секции .nominations
+    $('.btn-scroll-js').on('click',function (e) {
+        e.preventDefault();
+        var target = this.hash,
+            $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top - 55
+        }, 600, 'swing', function () {
+        });
+    });
+
+})(jQuery);
